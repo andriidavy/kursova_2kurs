f0587e06e952639573475138816c46f1ff697c98
andriidavy <davydenkoandriy5932@gmail.com>
2023-05-27 15:35:12 +0200

    pin for admin mode

--- app/src/main/java/com/example/registration/fragment/managerFragments/adminMode/AdminLoginFragment.kt
+++ app/src/main/java/com/example/registration/fragment/managerFragments/adminMode/AdminLoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.registration.R
 import com.example.registration.databinding.FragmentAdminLoginBinding
@@ -21,8 +22,14 @@ class AdminLoginFragment : Fragment() {
         val navController = findNavController()
 
         binding.loginButton.setOnClickListener {
-            navController.navigate(R.id.action_adminLoginFragment_to_adminMainPageFragment)
+            if (binding.etPinCode.text.toString() == "4308") {
+                navController.navigate(R.id.action_adminLoginFragment_to_adminMainPageFragment)
+            }
+            else {
+                Toast.makeText(context,"Пін-код невірний!",Toast.LENGTH_LONG).show()
+            }
         }
+
 
 
 

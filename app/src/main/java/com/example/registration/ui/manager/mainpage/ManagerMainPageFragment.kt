fc58006818f60c39bd0b88a91f872c8502c8ac9b
andriidavy <davydenkoandriy5932@gmail.com>
2023-07-25 17:25:51 +0200

    upd

--- app/src/main/java/com/example/registration/ui/manager/mainpage/ManagerMainPageFragment.kt
+++ app/src/main/java/com/example/registration/ui/manager/mainpage/ManagerMainPageFragment.kt
@@ -5,49 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.example.registration.R
 import com.example.registration.databinding.FragmentManagerMainPageBinding
 
 class ManagerMainPageFragment : Fragment() {
 private lateinit var binding: FragmentManagerMainPageBinding
+private lateinit var navController: NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentManagerMainPageBinding.inflate(inflater)
+        return binding.root
+    }
 
-        val navController = findNavController()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonToCreatedCustoms.setOnClickListener {
+        setupViews()
+        setListeners()
+    }
+
+    private fun setupViews(){
+        navController = findNavController()
+    }
+
+    private fun setListeners() = with(binding){
+        buttonToCreatedCustoms.setOnClickListener {
             navController.navigate(R.id.action_managerMainPageFragment_to_managerCreatedCustomsPageFragment)
         }
 
-        binding.buttonToWaitingReport.setOnClickListener {
+        buttonToWaitingReport.setOnClickListener {
             navController.navigate(R.id.action_managerMainPageFragment_to_managerReportsInWaitingFragment)
         }
 
-        binding.buttonToAllCustoms.setOnClickListener {
+        buttonToAllCustoms.setOnClickListener {
             navController.navigate(R.id.action_managerMainPageFragment_to_managerAllCustomsFragment)
         }
 
-        binding.buttonToProductList.setOnClickListener {
+        buttonToProductList.setOnClickListener {
             navController.navigate(R.id.action_managerMainPageFragment_to_allProductListFragment)
         }
 
-        binding.buttonToHr.setOnClickListener {
+        buttonToHr.setOnClickListener {
             navController.navigate(R.id.action_managerMainPageFragment_to_manageEmployeeFragment)
         }
 
-        binding.buttonToMyProfile.setOnClickListener {
+        buttonToMyProfile.setOnClickListener {
             navController.navigate(R.id.action_managerMainPageFragment_to_managerProfilePageFragment)
         }
 
-        binding.buttonToAdminMode.setOnClickListener {
+        buttonToAdminMode.setOnClickListener {
             navController.navigate(R.id.action_managerMainPageFragment_to_adminLoginFragment)
         }
-
-        return binding.root
     }
 
 }
